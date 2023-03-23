61fc121c77aec73cb1294bee067c0515e79b01f1
lucas <lpatronr@gmail.com>
2023-03-22 20:07:09 -0500

    feat: query handler for all the user shared groups

---
+++ src/server/api/routers/debtTable/queries/getAllShared/handler.ts
@@ -0,0 +1,35 @@
+import { protectedVerifiedProcedure } from "$/server/api/trpc";
+
+const getAllSharedHandler = protectedVerifiedProcedure.query(
+  async ({ ctx }) => {
+    const query = await ctx.prisma.debtTable.findMany({
+      where: {
+        collaborators: {
+          some: {
+            collaboratorId: ctx.session.user.id,
+            role: {
+              not: "OWNER",
+            },
+          },
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        createdAt: true,
+        _count: {
+          select: {
+            collaborators: true,
+          },
+        },
+      },
+    });
+
+    return {
+      debtTables: query,
+    };
+  }
+);
+
+export default getAllSharedHandler;
