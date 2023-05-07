95b4d7c47e6d3ec98cb719242a22b369d242d68d
lucas-patron <lpatronr@gmail.com>
2023-05-07 16:19:22 -0500

    feat(hook): use redirect session

--- src/components/layouts/auth-wrapper/auth-wrapper.tsx
+++ src/components/layouts/auth-wrapper/auth-wrapper.tsx
@@ -1,9 +1,9 @@
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { type FC, type ReactNode } from "react";
 import dynamic from "next/dynamic";
 import LoadingPage from "$/components/pages/loading-page";
-import { Pages } from "$/enums/pages";
+import { Pages } from "$/lib/enums/pages";
+import { useRedirectSession } from "$/hooks/use-redirect-session";
 
 type Props = {
   children: ReactNode;
@@ -11,17 +11,12 @@ type Props = {
 
 const AuthWrapper: FC<Props> = ({ children }) => {
   const router = useRouter();
-  const session = useSession({
-    required: true,
-    onUnauthenticated() {
-      void router.push(Pages.HOME);
-    },
-  });
+  const session = useRedirectSession();
 
   if (session.status === "loading") return <LoadingPage />;
 
   if (session.data?.user.emailVerified === null) {
-    void router.push("/auth/verify-email");
+    void router.push(Pages.ONBOARDING);
     return <LoadingPage />;
   }
 
---
+++ src/hooks/use-redirect-session/use-redirect-session.ts
@@ -0,0 +1,15 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { Pages } from "$/lib/enums/pages";
+
+function useRedirectSession(): ReturnType<typeof useSession> {
+  const router = useRouter();
+  return useSession({
+    required: true,
+    onUnauthenticated() {
+      void router.push(Pages.LOGIN);
+    },
+  });
+}
+
+export { useRedirectSession };
