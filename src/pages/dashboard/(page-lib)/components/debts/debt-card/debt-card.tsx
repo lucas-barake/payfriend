99c01c79d8b54c77049a02af2508ac15b4b9cb09
lucas <lpatronr@gmail.com>
2023-05-19 17:41:03 -0500

    feat: card styles

--- src/pages/dashboard/(page-lib)/components/debts/debt-card/debt-card.tsx
+++ src/pages/dashboard/(page-lib)/components/debts/debt-card/debt-card.tsx
@@ -1,12 +1,13 @@
 import React, { type FC } from "react";
-import { Button } from "$/components/ui/button";
 import { DateTime } from "luxon";
 import { type AppRouter } from "$/server/api/root";
-import { Calendar } from "lucide-react";
+import { CalendarIcon, EyeIcon } from "lucide-react";
 import { Skeleton } from "$/components/ui/skeleton";
 import { type inferProcedureOutput } from "@trpc/server";
 import { strTransformer } from "$/lib/utils/str-transformer";
 import { Avatar } from "$/components/ui/avatar";
+import { buttonVariants } from "$/components/ui/button";
+import { Badge } from "$/components/ui/badge";
 
 type Props = {
   debt: NonNullable<
@@ -23,20 +24,18 @@ const DebtCard: FC<Props> & {
       key={debt.id}
       className="flex flex-col gap-2 rounded-lg border border-border p-6 shadow-sm transition-colors duration-200 ease-in hover:bg-background-secondary/70"
     >
-      <div className="flex items-center justify-between gap-4 text-lg font-bold text-indigo-500 dark:text-indigo-400">
+      <div className="flex w-full items-center justify-between gap-4 text-lg font-bold text-indigo-500 dark:text-indigo-400">
         <span className="truncate">{debt.name}</span>
 
-        <div className="flex -space-x-2 overflow-hidden p-2">
-          {debt.borrowers?.map(({ user }) => (
-            <Avatar key={user.name}>
-              <Avatar.Image src={user.image ?? undefined} />
+        {debt.borrowers?.map(({ user }) => (
+          <Avatar key={user.name} className="h-7 w-7">
+            <Avatar.Image src={user.image ?? undefined} />
 
-              <Avatar.Fallback>
-                {user.name?.[0]?.toUpperCase() ?? "?"}
-              </Avatar.Fallback>
-            </Avatar>
-          ))}
-        </div>
+            <Avatar.Fallback>
+              {user.name?.[0]?.toUpperCase() ?? "?"}
+            </Avatar.Fallback>
+          </Avatar>
+        ))}
       </div>
 
       <span className="mb-6 mt-2 pr-2 lg:pr-3 xl:pr-6">
@@ -44,18 +43,23 @@ const DebtCard: FC<Props> & {
       </span>
 
       <div className="mt-auto flex w-full items-center justify-between">
-        <span className="flex items-center gap-1 text-sm">
-          <Calendar className="h-4 w-4" />
-          {DateTime.fromJSDate(debt.createdAt).toLocaleString({
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-          })}
-        </span>
+        <Badge variant="secondary">
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {DateTime.fromJSDate(debt.createdAt).toLocaleString(
+            DateTime.DATE_MED
+          )}
+        </Badge>
 
-        <Button color="indigo" type="button" size="sm">
-          Ver
-        </Button>
+        <div
+          className={buttonVariants({
+            size: "sm",
+            variant: "outline",
+            className: "flex items-center gap-1.5",
+          })}
+        >
+          <EyeIcon className="h-5 w-5" />
+          <span className="sr-only xs:not-sr-only">Ver</span>
+        </div>
       </div>
     </button>
   );
