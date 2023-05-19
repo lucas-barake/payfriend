abbde9173a64dc0fde4f6ff05298eaa295be768e
lucas <lpatronr@gmail.com>
2023-05-19 11:26:23 -0500

    feat: optimize mutation by using transaction and parametrized query
    - Combine count queries into a single parameterized raw query for efficiency
    - Wrap debt creation and pending invites creation in a transaction for atomicity
    - Improve data consistency and reduce database queries

--- src/server/api/routers/debts/mutations/index.ts
+++ src/server/api/routers/debts/mutations/index.ts
@@ -7,49 +7,47 @@ import CUSTOM_EXCEPTIONS from "$/server/api/custom-exceptions";
 import { getUserDebtsSelect } from "$/server/api/routers/user/debts/queries";
 
 export const debtsMutations = createTRPCRouter({
-  createGroup: protectedVerifiedProcedure
+  create: protectedVerifiedProcedure
     .input(createGroupInput)
     .mutation(async ({ ctx, input }) => {
-      const lenderDebtsCount = await ctx.prisma.debt.count({
-        where: {
-          lenderId: ctx.session.user.id,
-        },
-      });
-
-      const borrowerDebtsCount = await ctx.prisma.borrower.count({
-        where: {
-          userId: ctx.session.user.id,
-        },
-      });
-
-      const totalDebtsCount = lenderDebtsCount + borrowerDebtsCount;
+      const debtsCount: {
+        lenderDebtsCount: number;
+        borrowerDebtsCount: number;
+      } = await ctx.prisma.$queryRaw`
+          SELECT (SELECT COUNT(*) FROM "Debt" WHERE "lenderId" = ${ctx.session.user.id})   AS "lenderDebtsCount",
+                 (SELECT COUNT(*) FROM "Borrower" WHERE "userId" = ${ctx.session.user.id}) AS "borrowerDebtsCount"
+      `;
+      const totalDebtsCount =
+        debtsCount.lenderDebtsCount + debtsCount.borrowerDebtsCount;
 
       if (totalDebtsCount >= 5) {
         throw CUSTOM_EXCEPTIONS.BAD_REQUEST("No puedes tener más de 5 deudas");
       }
 
-      const createdDebt = await ctx.prisma.debt.create({
-        data: {
-          name: input.name,
-          description: input.description,
-          lender: {
-            connect: {
-              id: ctx.session.user.id,
+      return ctx.prisma.$transaction(async (prisma) => {
+        const createdDebt = await prisma.debt.create({
+          data: {
+            name: input.name,
+            description: input.description,
+            lender: {
+              connect: {
+                id: ctx.session.user.id,
+              },
             },
+            amount: input.amount,
           },
-          amount: input.amount,
-        },
-        select: getUserDebtsSelect,
-      });
+          select: getUserDebtsSelect,
+        });
 
-      await ctx.prisma.pendingInvite.createMany({
-        data: input.borrowerEmails.map((email) => ({
-          inviteeEmail: email,
-          debtId: createdDebt.id,
-          inviterId: ctx.session.user.id,
-        })),
-      });
+        await prisma.pendingInvite.createMany({
+          data: input.borrowerEmails.map((email) => ({
+            inviteeEmail: email,
+            debtId: createdDebt.id,
+            inviterId: ctx.session.user.id,
+          })),
+        });
 
-      return createdDebt;
+        return createdDebt;
+      });
     }),
 });
