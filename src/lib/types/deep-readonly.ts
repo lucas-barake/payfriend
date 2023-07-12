36b35f8d9b0ccb6d6efdf8c2a07d6866c7cb5679
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-07-12 01:44:55 -0500

    chore(eslint): fix warnings & errors

--- src/lib/types/deep-readonly.ts
+++ src/lib/types/deep-readonly.ts
@@ -1,12 +1,15 @@
+// eslint-disable-next-line no-use-before-define
+export type DeepReadonlyArray<T> = object & ReadonlyArray<DeepReadonly<T>>;
+
 export type DeepReadonly<T> = T extends Array<infer R>
   ? DeepReadonlyArray<R>
-  : T extends Function
+  : // eslint-disable-next-line @typescript-eslint/ban-types
+  T extends Function
   ? T
   : T extends object
-  ? DeepReadonlyObject<T>
+  ? // eslint-disable-next-line no-use-before-define
+    DeepReadonlyObject<T>
   : T;
-
-export type DeepReadonlyArray<T> = {} & ReadonlyArray<DeepReadonly<T>>;
 
 export type DeepReadonlyObject<T> = {
   readonly [P in keyof T]: DeepReadonly<T[P]>;
