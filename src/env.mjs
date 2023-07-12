dc115f563de0e2c8c5364ef5741ae3ebd802a8f0
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-07-12 01:45:12 -0500

    chore(env): add VERCEL_URL env variable

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ import { createEnv } from "@t3-oss/env-core";
 export const env = createEnv({
   clientPrefix: "NEXT_PUBLIC_",
   server: {
+    VERCEL_URL: z.string().url().optional(),
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
