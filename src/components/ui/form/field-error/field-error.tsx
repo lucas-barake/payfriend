7424167940b86462e06a766dee73e37dccdacb49
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-07-22 18:12:00 -0500

    feat(field-error): add hide prop

--- src/components/ui/form/field-error/field-error.tsx
+++ src/components/ui/form/field-error/field-error.tsx
@@ -3,11 +3,13 @@ import { cn } from "$/lib/utils/cn";
 
 type Props = {
   children: React.ReactNode | null | undefined;
+  hide?: unknown;
 } & React.ComponentPropsWithRef<"p">;
 
 export const FieldError = React.forwardRef<HTMLParagraphElement, Props>(
-  ({ children, className, ...props }, ref) => {
+  ({ children, className, hide, ...props }, ref) => {
     if (!children) return null;
+    if (Boolean(hide)) return null;
 
     return (
       <p
