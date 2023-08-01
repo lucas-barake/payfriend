455de7b322817fea8d68b77c5d46b22f407cb541
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-07-31 19:39:21 -0500

    chore: revert migration

--- prisma/migrations/20230801003048_subscriptions_and_migrate_cuids_to_uuids/migration.sql
+++ prisma/migrations/20230801003048_subscriptions_and_migrate_cuids_to_uuids/migration.sql
@@ -2,18 +2,20 @@
 CREATE TYPE "SubscriptionType" AS ENUM ('BASIC');
 
 -- CreateEnum
-CREATE TYPE "SubscriptionStatus" AS ENUM ('ACTIVE', 'PENDING', 'FAILED');
+CREATE TYPE "SubscriptionStatus" AS ENUM ('SUCCESS', 'PENDING', 'FAILED', 'CANCELLED');
 
 -- CreateTable
-CREATE TABLE "Subscription" (
+CREATE TABLE "ActiveSubscription" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ(6) NOT NULL,
     "userId" TEXT NOT NULL,
     "type" "SubscriptionType" NOT NULL,
-    "expires" TIMESTAMPTZ(6) NOT NULL,
+    "status" "SubscriptionStatus" NOT NULL,
+    "nextDueDate" TIMESTAMPTZ(6) NOT NULL,
+    "externalReference" TEXT NOT NULL,
 
-    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ActiveSubscription_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -22,17 +24,18 @@ CREATE TABLE "SubscriptionHistory" (
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
     "type" "SubscriptionType" NOT NULL,
-    "expires" TIMESTAMPTZ(6) NOT NULL,
     "status" "SubscriptionStatus" NOT NULL,
+    "nextDueDate" TIMESTAMPTZ(6) NOT NULL,
+    "externalReference" TEXT NOT NULL,
 
     CONSTRAINT "SubscriptionHistory_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Subscription_userId_key" ON "Subscription"("userId");
+CREATE UNIQUE INDEX "ActiveSubscription_userId_key" ON "ActiveSubscription"("userId");
 
 -- AddForeignKey
-ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ActiveSubscription" ADD CONSTRAINT "ActiveSubscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "SubscriptionHistory" ADD CONSTRAINT "SubscriptionHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
