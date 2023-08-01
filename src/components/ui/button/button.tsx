199f8a018660db3b7bad98346cc81ba9e4d3bb7d
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-08-01 14:35:28 -0500

    feat(button): ring variants

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -11,9 +11,11 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        success: "bg-success text-success-foreground hover:bg-success/90",
-        warning: "bg-warning text-warning-foreground hover:bg-warning/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 focus-visible:ring-destructive",
+        success:
+          "bg-success text-success-foreground hover:bg-success/90 focus-visible:ring-success",
+        warning:
+          "bg-warning text-warning-foreground hover:bg-warning/90 focus-visible:ring-warning",
         outline:
           "border border-input hover:bg-accent hover:text-accent-foreground",
         secondary:
