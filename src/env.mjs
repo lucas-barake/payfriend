678d9b77f10ef9aa652541972f04c0283b2e0709
lucas <86905052+lucas-barake@users.noreply.github.com>
2023-08-05 03:12:25 -0500

    fix: add PRIMARY_DOMAIN to runtime env definition

--- src/env.mjs
+++ src/env.mjs
@@ -55,6 +55,7 @@ export const env = createEnv({
     VERCEL_URL: process.env.VERCEL_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    PRIMARY_DOMAIN: process.env.PRIMARY_DOMAIN,
     /* NextAuth */
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
